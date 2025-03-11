ec73b87be60e7944d3ed18a356e93c432145a409
Mohamed Ayman <mohammed.a.alhadad@gmail.com>
2025-03-11 05:52:52 +0200

    validation login

--- lib/Login_screen.dart
+++ lib/Login_screen.dart
@@ -9,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool isVisible = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,63 +20,102 @@ class _LoginScreenState extends State<LoginScreen> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "User Login",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 60),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "User Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 40),
 
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              obscureText: isVisible,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: IconButton(
-                  icon:
-                      isVisible
-                          ? Icon(Icons.visibility)
-                          : Icon(Icons.visibility_off),
-                  onPressed: () {
-                    setState(() {
-                      isVisible = !isVisible;
-                    });
+                TextFormField(
+                  validator: (value) {
+                    final bool emailValid = RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                    ).hasMatch(value!);
+                    if (!emailValid) {
+                      return "Please enter a valid email";
+                    }
+                    if (value!.isEmpty) {
+                      return "Please enter your email";
+                    }
+                    return null;
                   },
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(onPressed: () {}, child: Text("Login")),
+                SizedBox(height: 20),
+                TextFormField(
+                  validator: (value) {
+                    final bool passwordValid = RegExp(
+                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$',
+                    ).hasMatch(value!);
+                    if (!passwordValid) {
+                      return "Please enter a valid password";
+                    }
+                    if (value!.isEmpty) {
+                      return "Please enter your password";
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  obscureText: isVisible,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon:
+                          isVisible
+                              ? Icon(Icons.visibility)
+                              : Icon(Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          isVisible = !isVisible;
+                        });
+                      },
+                    ),
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            print(emailController.text);
+                            print(passwordController.text);
+                          }
+                        },
+                        child: Text("Login"),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(onPressed: () {}, child: Text("Sign Up")),
+                  ],
                 ),
               ],
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?"),
-                TextButton(onPressed: () {}, child: Text("Sign Up")),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
